967a81832ef343d17410aab5037bb6ce0f2850bf
Nico <38060004+nicokinazoko@users.noreply.github.com>
2025-01-30 10:36:57 +0700

    [HackerRank][Submitted Code] Solution for day of programmer

---
+++ 1. Problem Solving/1. Algorithm/Implementations/8-day-of-programmer.js
@@ -0,0 +1,21 @@
+function dayOfProgrammer(year) {
+  // Write your code here
+  let leapDate;
+
+  if (year < 1918) {
+    leapDate = `${year % 4 === 0 ? 12 : 13}.09.${year}`;
+  } else if (year > 1918) {
+    leapDate = `${
+      (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0 ? 12 : 13
+    }.09.${year}`;
+  } else {
+    leapDate = `26.09.${year}`;
+  }
+  return leapDate;
+}
+
+console.log(dayOfProgrammer(2017));
+console.log(dayOfProgrammer(2016));
+console.log(dayOfProgrammer(1800));
+console.log(dayOfProgrammer(2100));
+console.log(dayOfProgrammer(1917));
