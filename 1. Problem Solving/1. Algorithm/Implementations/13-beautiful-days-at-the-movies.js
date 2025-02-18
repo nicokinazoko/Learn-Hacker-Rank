b6c86dc93c79b3ab89f27222eaed7b5720e1cbfa
Nico <38060004+nicokinazoko@users.noreply.github.com>
2025-02-18 11:50:46 +0700

    [HackerRank][Submitted Code] Solution for Beautiful Days at the Movies

---
+++ 1. Problem Solving/1. Algorithm/Implementations/13-beautiful-days-at-the-movies.js
@@ -0,0 +1,17 @@
+function beautifulDays(i, j, k) {
+  // Write your code here
+  let countBeautifulDays = 0;
+  for (let iteration = i; iteration <= j; iteration++) {
+    if (
+      Math.abs(iteration - +String(iteration).split('').reverse().join('')) %
+        k ===
+      0
+    ) {
+      countBeautifulDays++;
+    }
+  }
+
+  return countBeautifulDays;
+}
+
+console.log(beautifulDays(20, 23, 6));
